6f0a51c542b8d1a9ec92ea1c8e46359203634688
Jiachen ZHANG <jzhangea@connect.ust.hk>
2025-02-26 13:59:20 +0800

    basic version

---
+++ guess.py
@@ -0,0 +1,18 @@
+import random
+
+r = random.randint (1,100)
+
+while True:
+	num = input ('Please input a number between 1 and 100: ')
+	num = int(num)
+
+	if num == r:
+		print('You have guessed correctly!')
+		break
+
+	elif num < r:
+		print('Your guess is smaller than the true number.')
+
+	else:
+		print('Your guess if larger than the true number')
+
